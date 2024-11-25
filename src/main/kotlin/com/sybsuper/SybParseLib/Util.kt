eeba8f2a2e3e398cb4290557ca360294ff092178
Sybsuper <sybsuper@gmail.com>
2024-11-25 18:21:20 +0100

    fix: missing dokka build in github workflow

--- src/main/kotlin/com/sybsuper/SybParseLib/Util.kt
+++ src/main/kotlin/com/sybsuper/SybParseLib/Util.kt
@@ -1,11 +1,8 @@
 package com.sybsuper.SybParseLib
 
-import java.util.*
-
 
 fun <S> List<S>.startsWith(otherList: List<S>): Boolean {
-    if (otherList.size > this.size)
-        return false
+    if (otherList.size > this.size) return false
 
     return otherList.indices.all { this[it] == otherList[it] }
 }
